baecd02752dd0c5c5bbf221beee822754b529430
Marco Micera <marco.micera@gmail.com>
2020-04-27 08:19:15 +0200

    Added sbt, Java 8+11, Scala, to the essential programs list

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,10 +17,13 @@ doPrograms() {
 
     PROGRAMS_TO_INSTALL=(
         curl
-        # dconf-editor
+        dconf-editor
         # git
         tldr
         texlive-latex-base
+        openjdk-8-jdk-headless
+        openjdk-11-jdk-headless
+        scala
     )
 
     echo "Installing essential programs like ${PROGRAMS_TO_INSTALL[@]}..."
@@ -29,6 +32,14 @@ doPrograms() {
         sudo apt install -y $PROGRAM_TO_INSTALL
         echo "...$PROGRAM_TO_INSTALL installed."
     done
+
+    # sbt
+    echo "Installing sbt..."
+    echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+    curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+    sudo apt update
+    sudo apt install -y sbt
+    echo "...sbt installed."
 }
 
 doInstall() {

a8bed1e0f67cca84a977e21ed60932c2240f41e8
Marco Micera <marco.micera@gmail.com>
2020-06-29 21:18:04 +0200

    Updated fish config file for smsng TV

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -61,7 +61,8 @@ function monitor
     echo "Adjusting notebook's display..."
     set notebook_display (_get_display_name LVDS)
     # xrandr --output $notebook_display --pos 0x435 # black stand
-    xrandr --output $notebook_display --pos 0x170 # white stand
+    # xrandr --output $notebook_display --pos 0x170 # white stand
+    xrandr --output $notebook_display --pos 0x40 # old smsng TV
 end
 function monitors-no-base
     monitor

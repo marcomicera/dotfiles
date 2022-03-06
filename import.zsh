e2815b3b99de31013d28a15e67e88e691ef9be41
Marco Micera <marco.micera@gmail.com>
2022-03-06 20:00:54 +0100

    Added iTerm2 shell integration script

--- import.zsh
+++ import.zsh
@@ -22,7 +22,8 @@ magenta "zsh"
   cp ~/.zshrc "${CWD}"/zsh
   cp ~/.p10k.zsh "${CWD}"/zsh
   cp ~/.hushlogin "${CWD}"/zsh # disabled "last login" prompt
-  cp ~/.config/iterm2/settings/com.googlecode.iterm2.plist "${CWD}"/zsh # iTerm2 settings 
+  cp ~/.config/iterm2/settings/com.googlecode.iterm2.plist "${CWD}"/zsh # iTerm2 settings
+  cp ~/.iterm2_shell_integration.zsh "${CWD}"/zsh # https://iterm2.com/documentation-shell-integration.html
 )
 
 # Ruby

849d5d1425b5318f3687b0905c174c7ec13029be
Marco Micera <marco.micera@gmail.com>
2022-05-03 16:26:50 +0200

    Fixed fzf script to use $HOME

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/marco.micera/.asdf/installs/fzf/0.29.0/bin* ]]; then
+if [[ ! "$PATH" == *${HOME}/.asdf/installs/fzf/0.29.0/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/Users/marco.micera/.asdf/installs/fzf/0.29.0/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/marco.micera/.asdf/installs/fzf/0.29.0/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.asdf/installs/fzf/0.29.0/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/marco.micera/.asdf/installs/fzf/0.29.0/shell/key-bindings.zsh"
+source "${HOME}/.asdf/installs/fzf/0.29.0/shell/key-bindings.zsh"

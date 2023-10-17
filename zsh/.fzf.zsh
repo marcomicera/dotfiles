49d33c1e40a5a694afb58009c651278b47c01567
Marco Micera <marco.micera@gmail.com>
2023-10-17 11:29:36 +0200

    Update `fzf`

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *${HOME}/.asdf/installs/fzf/0.29.0/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/marco.micera/.asdf/installs/fzf/0.29.0/bin"
+if [[ ! "$PATH" == */Users/micera/.asdf/installs/fzf/0.43.0/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/Users/micera/.asdf/installs/fzf/0.43.0/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "${HOME}/.asdf/installs/fzf/0.29.0/shell/completion.zsh" 2> /dev/null
+source "/Users/micera/.asdf/installs/fzf/0.43.0/shell/completion.zsh"
 
 # Key bindings
 # ------------
-source "${HOME}/.asdf/installs/fzf/0.29.0/shell/key-bindings.zsh"
+source "/Users/micera/.asdf/installs/fzf/0.43.0/shell/key-bindings.zsh"

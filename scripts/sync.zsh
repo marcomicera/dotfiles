3f5d6e7c16711200b573b71a6aefbbd52d889dbd
Marco Micera <marco.micera@gmail.com>
2025-09-04 20:46:57 +0200

    Sync scripts skips absent files

--- scripts/sync.zsh
+++ scripts/sync.zsh
@@ -37,20 +37,20 @@ magenta "zsh"
   symlink "${CWD}"/zsh ~/.zshrc
   symlink "${CWD}"/zsh ~/.p10k.zsh
   symlink "${CWD}"/zsh ~/.hushlogin # disabled "last login" prompt
-  symlink "${CWD}"/zsh ~/.config/iterm2/settings/com.googlecode.iterm2.plist  # iTerm2 settings
+  # symlink "${CWD}"/zsh ~/.config/iterm2/settings/com.googlecode.iterm2.plist  # iTerm2 settings
   symlink "${CWD}"/zsh ~/.iterm2_shell_integration.zsh # https://iterm2.com/documentation-shell-integration.html
   symlink "${CWD}"/zsh ~/.fzf.zsh 
   symlink "${CWD}"/.config/bat ~/.config/bat/config 
 )
 
 # JetBrains IDEs
-magenta "JetBrains IDEs"
-(
-  set -x
-  symlink "${CWD}" /usr/local/bin/idea
-  symlink "${CWD}" /usr/local/bin/storm
-  symlink "${CWD}" /usr/local/bin/charm
-)
+# magenta "JetBrains IDEs"
+# (
+#   set -x
+#   symlink "${CWD}" /usr/local/bin/idea
+#   symlink "${CWD}" /usr/local/bin/storm
+#   symlink "${CWD}" /usr/local/bin/charm
+# )
 
 # Oh My Zsh
 magenta "Oh My Zsh"
@@ -83,19 +83,19 @@ magenta "asdf"
 )
 
 # git
-magenta "git"
-(
-  set -x
-  symlink "${CWD}"/git ~/.gitconfig
-  symlink "${CWD}"/.config/git ~/.config/git/ignore
-  symlink "${CWD}"/.gnupg ~/.gnupg/gpg*.conf 
-)
+# magenta "git"
+# (
+#   set -x
+#   symlink "${CWD}"/git ~/.gitconfig
+#   symlink "${CWD}"/.config/git ~/.config/git/ignore
+#   symlink "${CWD}"/.gnupg ~/.gnupg/gpg*.conf 
+# )
 
-# SmartGit
-magenta "SmartGit"
-(
-  symlink "${CWD}" /usr/local/bin/sm
-)
+# # SmartGit
+# magenta "SmartGit"
+# (
+#   symlink "${CWD}" /usr/local/bin/sm
+# )
 
 # nano
 magenta "nano"
@@ -112,24 +112,24 @@ magenta "Spotify TUI"
 )
 
 # vim
-magenta "vim"
-(
-  VIM_RUNTIME=~/.vim_runtime
-  VIM_RUNTIME_CONFIGS="${VIM_RUNTIME}"/my_configs.vim
-  if [ ! -f "${VIM_RUNTIME_CONFIGS}" ]; then
-    red "Custom vim config file at ${VIM_RUNTIME_CONFIGS} not found."
-    printf "Install github.com/amix/vimrc with:\n"
-    printf "\t git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime\n"
-    printf "\t sh ~/.vim_runtime/install_awesome_vimrc.sh\n"
-    printf "\t vim ${VIM_RUNTIME_CONFIGS}\n\n"
-    exit 1
-  else
-    set -x
+# magenta "vim"
+# (
+#   VIM_RUNTIME=~/.vim_runtime
+#   VIM_RUNTIME_CONFIGS="${VIM_RUNTIME}"/my_configs.vim
+#   if [ ! -f "${VIM_RUNTIME_CONFIGS}" ]; then
+#     red "Custom vim config file at ${VIM_RUNTIME_CONFIGS} not found."
+#     printf "Install github.com/amix/vimrc with:\n"
+#     printf "\t git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime\n"
+#     printf "\t sh ~/.vim_runtime/install_awesome_vimrc.sh\n"
+#     printf "\t vim ${VIM_RUNTIME_CONFIGS}\n\n"
+#     exit 1
+#   else
+#     set -x
 
-    # Config
-    symlink "${CWD}"/vim/ "${VIM_RUNTIME_CONFIGS}"
-  fi
-)
+#     # Config
+#     symlink "${CWD}"/vim/ "${VIM_RUNTIME_CONFIGS}"
+#   fi
+# )
 
 # VSCodium
 magenta "VSCodium"
@@ -139,21 +139,21 @@ magenta "VSCodium"
   symlink "${CWD}"/codium ~/Library/Application\ Support/VSCodium/User/keybindings.json
 )
 
-# k9s
-if [ -n ${XDG_CONFIG_HOME} ]; then 
-  magenta "k9s"
-  (
-    set -x
-    symlink "${CWD}"/.config/k9s ${XDG_CONFIG_HOME}/k9s/plugin.yml
-  )
-fi
+# # k9s
+# if [ -n ${XDG_CONFIG_HOME} ]; then 
+#   magenta "k9s"
+#   (
+#     set -x
+#     symlink "${CWD}"/.config/k9s ${XDG_CONFIG_HOME}/k9s/plugin.yml
+#   )
+# fi
 
-# Rectangle
-# https://github.com/rxhanson/Rectangle
-magenta "Rectangle"
-(
-  set -x
-  symlink "${CWD}"/.config/rectangle ~/Library/Application\ Support/Rectangle/RectangleConfig.json
-)
+# # Rectangle
+# # https://github.com/rxhanson/Rectangle
+# magenta "Rectangle"
+# (
+#   set -x
+#   symlink "${CWD}"/.config/rectangle ~/Library/Application\ Support/Rectangle/RectangleConfig.json
+# )
 
 printf "\n"

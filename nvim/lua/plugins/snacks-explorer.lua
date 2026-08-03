f984a9a240aa0a6dcaa0161efe19426aa401d7bc
Marco Micera <marco.micera@gmail.com>
2026-08-03 12:00:22 +0200

    nvim file finder includes hidden files

--- nvim/lua/plugins/snacks-explorer.lua
+++ nvim/lua/plugins/snacks-explorer.lua
@@ -45,6 +45,9 @@ return {
         explorer = {
           hidden = true,
         },
+        files = {
+          hidden = true,
+        },
         grep = {
           hidden = true,
         },

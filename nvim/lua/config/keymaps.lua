eef6656676529b475f329540b3e1924965c8047d
Marco Micera <marco.micera@gmail.com>
2026-08-03 18:11:43 +0200

    LazyGit in nvim's homepage

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+if vim.fn.executable("lazygit") == 1 then
+  vim.keymap.set("n", "z", function()
+    Snacks.lazygit({ cwd = LazyVim.root.git() })
+  end, { desc = "Lazygit" })
+end
--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -1,9 +1,18 @@
 return {
   "folke/snacks.nvim",
-  opts = {
-    lazygit = {
+  opts = function(_, opts)
+    opts.lazygit = vim.tbl_deep_extend("force", opts.lazygit or {}, {
       -- sync theme with nvim colorscheme; open files in this nvim instance
       configure = true,
-    },
-  },
+    })
+
+    if vim.fn.executable("lazygit") == 1 then
+      table.insert(opts.dashboard.preset.keys, 5, {
+        icon = "󰊢 ",
+        key = "z",
+        desc = "Lazygit",
+        action = ":lua Snacks.lazygit({ cwd = LazyVim.root.git() })",
+      })
+    end
+  end,
 }
